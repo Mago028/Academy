b53651fe7e6689ad700bf09ae7902e9353cba3d8
Yeojin Park <contrail020812@naver.com>
2023-02-03 03:31:44 +0900

    Initial commit

---
+++ Mentor_5.py
@@ -0,0 +1,36 @@
+# 5강
+# P.99
+# 1번
+score = int(input('점수를 입력하세요>> '))
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+print(f'점수는 {score}점이고, 학점은 {grade}학점입니다.')
+
+# 2번
+num = int(input('정수를 입력하세요>> '))
+print(f'{num}는 3의', '배수입니다.' if num % 3 == 0 else '배수가 아닙니다.')
+
+# 3번
+n1 = int(input('정수1 입력>> '))
+n2 = int(input('정수2 입력>> '))
+n3 = int(input('정수3 입력>> '))
+if n1 > n2 and n1 > n3:
+    print(f'가장 큰 수는 {n1}입니다.')
+elif n2 > n1 and n2 > n3:
+    print(f'가장 큰 수는 {n2}입니다.')
+elif n3 > n1 and n3 > n2:
+    print(f'가장 큰 수는 {n3}입니다.')
+else:
+    print('같은 수를 입력했습니다!')
+
+# 4번
+car_number = input('차량번호를 입력하세요>> ')
+print(f'차량번호 "{car_number}"는 오늘', '운행가능입니다.' if int(car_number[-1]) % 2 == 0 else '운행불가입니다.')
