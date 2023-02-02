e1eb3c80063243b87c85159689a51328c8465100
Yeojin Park <contrail020812@naver.com>
2023-02-03 01:41:50 +0900

    Initial commit

--- Mentor_1.py
+++ Mentor_1.py
@@ -6,3 +6,7 @@ print(f'{student[0]}학년 {student[1:3]}번 {student[3:]}번')
 # 2번
 car_num = '서울2가1234'
 print(f'{car_num}의 차량번호 4자리는 {car_num[-4:]}입니다.')
+
+# 3번
+s = 'maple'
+print(f'{s}의 가운데 글자는 {s[len(s)//2]}')
